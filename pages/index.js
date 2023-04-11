6bf194105c452aecde72b178a1c76d6d242c3462
Alex <user@user.com>
2023-04-11 16:46:28 +0300

    Корретировки в index.js 	modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import SearchOnOMDb from '../components/SearchOnOMDb'
-import Head from 'next/Head';
 
 export default function App() {
     return <>
-        <Head>
-            <title>Search On OMDb</title>
-        </Head>
         <SearchOnOMDb />
     </>
 }
