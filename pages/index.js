6a1940eed86a392cfbfa4d2caa69796d5dc50beb
unknown <aleksejignatov088@gmail.com>
2023-05-14 22:22:40 +0300

    Добавлен title 	modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import SearchOnOMDb from '../components/SearchOnOMDb'
+import Head from 'next/head';
 
 export default function App() {
     return <>
+        <Head>
+            <title>Movies from OMDB</title>
+        </Head>
         <SearchOnOMDb />
     </>
 }
